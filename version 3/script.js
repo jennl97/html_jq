0487336b7610c3488fe464685c437f3b4e113b15
jennl97 <jennhott97@gmail.com>
2016-11-01 19:31:14 -0500

    Version 3

---
+++ version 3/script.js
@@ -0,0 +1,21 @@
+/**
+ * Created by JENN on 10/31/2016.
+ */
+
+$(document).ready(function () {
+	"use strict";
+    $("#button").click(function () {
+        var toAdd = $("input[name=checkListForm]").val();
+        $("#list").append('<div class="item">' + toAdd +'</div>');
+    });
+	
+	$("#button").bind("click", function() {
+		$("input[type=text], textarea").val("");
+	});
+
+    $(document).on("click",".item",function () {
+        $(this).remove();
+
+    });
+    
+});
